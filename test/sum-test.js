d3846bbe6234ea74dea2c48b65640f9e7b3a0498
Sol Laudon <blaudon1@gmail.com>
2017-07-05 13:44:45 -0600

    add tests

--- test/sum-test.js
+++ test/sum-test.js
@@ -14,5 +14,32 @@ describe ('Sum', function() {
       expect(sum()).to.equal(0);
       assert.equal(sum(),0);
     });
-    
+
+    it('Should add positive numbers',function(){
+      assert.equal(sum(1,2),3);
+      assert.equal(sum(1,2,13),16);
+    });
+
+    it('Should add positive and negative numbers',function(){
+      assert.equal(sum(1,-2),1);
+      assert.equal(sum(1,2,-13),-10);
+    });
+
+    it('Should add numbers with decimals',function(){
+      assert.equal(sum(1,3.5),4.5);
+      assert.equal(sum(4,0.7),4.7);
+    });
+
+    it('Should return undefined if arguement is not a number', function(){
+      assert.equal(sum(1,'a'),undefined);
+      assert.equal(sum('2',2),undefined);
+      assert.equal(sum([1],2),undefined);
+      assert.equal(sum(5,true),undefined);
+      assert.equal(sum(5,''),undefined);
+    });
+
+    it('Should return null is no arguement has been passed in',function(){
+      assert.equal(sum(2),null);
+      assert.equal(sum(3,null),null);
+    });
 });
